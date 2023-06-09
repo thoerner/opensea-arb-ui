55b47dbb1cb8d31106dbb84151a331e20c8e1207
Tim Hoerner <thoerner@gmail.com>
2023-06-08 22:48:44 -0400

    added toasts

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getRequest, postRequest } from './api'
 import ActiveScans from './components/ActiveScans'
 import StartScan from './components/StartScan'
 import ArbLogo from './assets/arb.png'
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   const [activeScans, setActiveScans] = useState([])
@@ -18,17 +19,28 @@ function App() {
   }, [getData])
 
   async function stopScan(slug) {
-    await postRequest('/stop', { collectionSlug: slug })
+    const response = await postRequest('/stop', { collectionSlug: slug })
+    if (response.error) {
+      toast.error(response.error)
+    } else {
+      toast.success(response)
+    }
     getData()
   }
 
   async function startScan(slug, margin, increment) {
-    await postRequest('/start', { collectionSlug: slug, margin: Number(margin), increment: Number(increment) })
+    const response = await postRequest('/start', { collectionSlug: slug, margin: Number(margin), increment: Number(increment) })
+    if (response.error) {
+      toast.error(response.error)
+    } else {
+      toast.success(response)
+    }
     getData()
   }
 
   return (
     <div className="App">
+      <Toaster />
       <img src={ArbLogo} alt="Arb Anderson" />
       <StartScan startScan={startScan} />
       <ActiveScans activeScans={activeScans} stopScan={stopScan} />
