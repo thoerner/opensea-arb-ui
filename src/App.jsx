8d7f879971cd838dfc6271f37c5cee41096eea87
Tim Hoerner <thoerner@gmail.com>
2023-06-12 17:05:41 -0400

    added 'fetch collection info' button

--- src/App.jsx
+++ src/App.jsx
@@ -18,31 +18,36 @@ function App() {
     getData()
   }, [getData])
 
-  async function stopScan(slug) {
-    const response = await postRequest('/stop', { collectionSlug: slug })
+  function toastResponse(response) {
     if (response.error) {
       toast.error(response.error)
     } else {
       toast.success(response)
     }
+  } 
+
+  async function stopScan(slug) {
+    const response = await postRequest('/stop', { collectionSlug: slug })
+    toastResponse(response)
     getData()
   }
 
   async function startScan(slug, margin, increment) {
     const response = await postRequest('/start', { collectionSlug: slug, margin: Number(margin), increment: Number(increment) })
-    if (response.error) {
-      toast.error(response.error)
-    } else {
-      toast.success(response)
-    }
+    toastResponse(response)
     getData()
+  }
+
+  async function getCollectionInfo(slug) {
+    const response = await postRequest('/collectionInfo', { collectionSlug: slug })
+    return response
   }
 
   return (
     <div className="App">
       <Toaster />
       <img src={ArbLogo} alt="Arb Anderson" />
-      <StartScan startScan={startScan} />
+      <StartScan startScan={startScan} fetchCollectionInfo={getCollectionInfo} />
       <ActiveScans activeScans={activeScans} stopScan={stopScan} />
     </div>
   )
--- src/components/StartScan.jsx
+++ src/components/StartScan.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
-const StartScan = ({ startScan }) => {
+const StartScan = ({ startScan, fetchCollectionInfo }) => {
   const [slug, setSlug] = useState('')
   const [margin, setMargin] = useState('0.15')
   const [increment, setIncrement] = useState('0.01')
+  const [collectionInfo, setCollectionInfo] = useState(null)
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -13,9 +14,31 @@ const StartScan = ({ startScan }) => {
     }
   }
 
+  const handleFetchInfo = async (event) => {
+    event.preventDefault()
+    if (slug !== '') {
+      const info = await fetchCollectionInfo(slug)
+      setCollectionInfo(info)
+    }
+  }
+
+  const RenderObject = ({ object }) => {
+    return Object.keys(object).map(key => {
+      const value = object[key];
+      return (
+        <div key={key}>
+          <strong>{key}: </strong>
+          {typeof value === 'object' && value !== null
+            ? <RenderObject object={value} />
+            : JSON.stringify(value)}
+        </div>
+      );
+    });
+  };
+
   return (
     <div id="startScan">
-        <form id="inputForm" onSubmit={handleSubmit}>
+      <form id="inputForm" onSubmit={handleSubmit}>
         <label className='formInput'>
             Collection Slug:{" "}
             <input 
@@ -43,8 +66,17 @@ const StartScan = ({ startScan }) => {
             placeholder="Enter increment"
             />
         </label>{" "}
+        <button type="button" onClick={handleFetchInfo}>Fetch Collection Info</button>
         <button type="submit">Start Scan</button>
-        </form>
+      </form>
+      <div>
+        {collectionInfo && <pre>
+          {collectionInfo.contractInfo.name}<br/><br/>
+          Traits:
+          <RenderObject object={collectionInfo.traits} /><br/>
+          Floor: {collectionInfo.stats.floor_price}
+        </pre>}
+      </div>
     </div>
   )
 }
