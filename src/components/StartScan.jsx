0c512fa5fe0e69bdae6b090504b64371758bc71a
Tim Hoerner <thoerner@gmail.com>
2023-06-14 15:37:12 -0400

    put traits in a dropdown

--- src/components/StartScan.jsx
+++ src/components/StartScan.jsx
@@ -36,36 +36,57 @@ const StartScan = ({ startScan, fetchCollectionInfo }) => {
     });
   };
 
+  const keysToArray = (object) => {
+    return Object.keys(object)
+  }
+
+  const RenderDropdown = ({ array }) => {
+    return array.map(key => {
+      return (
+        <option key={key} value={key}>{key}</option>
+      );
+    });
+  };
+
   return (
     <div id="startScan">
       <form id="inputForm" onSubmit={handleSubmit}>
-        <label className='formInput'>
-            Collection Slug:{" "}
-            <input 
-            type="text"
-            value={slug}
-            onChange={event => setSlug(event.target.value)}
-            placeholder="Enter collection slug"
-            />
-        </label>{" "}
-        <label className='formInput'>
-            Margin:{" "}
-            <input
-            type="text"
-            value={margin}
-            onChange={event => setMargin(event.target.value)}
-            placeholder="Enter margin"
-            />
-        </label>{" "}
-        <label className='formInput'>
-            Increment:{" "}
-            <input
-            type="text"
-            value={increment}
-            onChange={event => setIncrement(event.target.value)}
-            placeholder="Enter increment"
-            />
-        </label>{" "}
+        <div>
+          <label className='formInput'>
+              Collection Slug:{" "}
+              <input 
+              type="text"
+              value={slug}
+              onChange={event => setSlug(event.target.value)}
+              placeholder="Enter collection slug"
+              style={{width: '175px'}}
+              />
+          </label>
+        </div>
+        <div>
+          <label className='formInput'>
+              Margin:{" "}
+              <input
+              type="text"
+              value={margin}
+              onChange={event => setMargin(event.target.value)}
+              placeholder="Enter margin"
+              style={{width: '100px'}}
+              />
+          </label>
+        </div>
+        <div>
+          <label className='formInput'>
+              Increment:{" "}
+              <input
+              type="text"
+              value={increment}
+              onChange={event => setIncrement(event.target.value)}
+              placeholder="Enter increment"
+              style={{width: '100px'}}
+              />
+          </label>
+        </div>
         <button type="button" onClick={handleFetchInfo}>Fetch Collection Info</button>
         <button type="submit">Start Scan</button>
       </form>
@@ -74,8 +95,11 @@ const StartScan = ({ startScan, fetchCollectionInfo }) => {
           <a href={`https://opensea.io/collection/${slug}`} target='_blank' rel="noreferrer">{collectionInfo.name}</a><br/><br/>
           {collectionInfo.creatorFee.isEnforced ? <><span style={{color: 'red'}}>Creator Fee enforced!</span> <span style={{color: 'yellow'}}>{collectionInfo.creatorFee.fee / 100}%</span></> : <span style={{color: 'darkgreen'}}>Creator Fee is not enforced</span>}<br/>
           <span style={{color: 'purple'}}>Floor: {collectionInfo.stats.floor_price}</span><br/>
-          Traits:
-          <RenderObject object={collectionInfo.traits} />
+          <span style={{color: 'orange'}}>Trait:</span>
+          <select name="traits" id="traits">
+            <option key="All" value="All">All</option>
+            <RenderDropdown array={keysToArray(collectionInfo.traits)} />
+          </select>
         </>}
       </div>
     </div>
