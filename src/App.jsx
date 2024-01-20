8b4fdd4e9fe1aba91fbed60ea77aeaa470cb6314
Tim Hoerner <thoerner@gmail.com>
2024-01-20 11:56:11 -0500

    add password

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import StartScan from "./components/StartScan";
 import ArbLogo from "./assets/arb.png";
 import toast, { Toaster } from "react-hot-toast";
 
+const PASSWORD = import.meta.env.VITE_PASSWORD;
+
 function App() {
   const [activeScans, setActiveScans] = useState([]);
+  const [authorized, setAuthorized] = useState(false);
 
   const refreshScans = useCallback(async () => {
     const active = await getRequest("/active");
@@ -64,42 +67,85 @@ function App() {
   return (
     <div className="App">
       <Toaster />
-      <div
-        style={{
-          display: "flex",
-          alignItems: "flex-end",
-          justifyContent: "space-between",
-          width: "50vw",
-        }}
-      >
-        <p
-          style={{
-            fontSize: "1.7rem",
-            transform: "translateY(1.5rem)",
-          }}
-        >Arb Anderson <span style={{fontSize: "1.1rem"}}>v1.0.2</span></p>
-        <a
-          href="https://opensea.io/ArbAnderson"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            src={ArbLogo}
-            alt="Arb Anderson"
-            style={{ transform: "translateY(7px)", width: "100px" }}
+      {authorized ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "space-between",
+              width: "50vw",
+            }}
+          >
+            <p
+              style={{
+                fontSize: "1.7rem",
+                transform: "translateY(1.5rem)",
+              }}
+            >
+              Arb Anderson <span style={{ fontSize: "1.1rem" }}>v1.0.2</span>
+            </p>
+            <a
+              href="https://opensea.io/ArbAnderson"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={ArbLogo}
+                alt="Arb Anderson"
+                style={{ transform: "translateY(7px)", width: "100px" }}
+              />
+            </a>
+            <div></div>
+          </div>
+          <StartScan
+            startScan={startScan}
+            fetchCollectionInfo={getCollectionInfo}
           />
-        </a>
-        <div></div>
-      </div>
-      <StartScan
-        startScan={startScan}
-        fetchCollectionInfo={getCollectionInfo}
-      />
-      <ActiveScans
-        activeScans={activeScans}
-        stopScan={stopScan}
-        fetchCollectionInfo={getCollectionInfo}
-      />
+          <ActiveScans
+            activeScans={activeScans}
+            stopScan={stopScan}
+            fetchCollectionInfo={getCollectionInfo}
+          />
+        </>
+      ) : (
+        <>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "50vw",
+            }}
+          >
+            <a
+              href="https://opensea.io/ArbAnderson"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={ArbLogo}
+                alt="Arb Anderson"
+                style={{ transform: "translateY(7px)", width: "100px" }}
+              />
+            </a>
+            <div></div>
+          </div>
+          <div style={{ width: "50vw", backgroundColor: "white", color: "black", padding: "20px", borderRadius: "1rem", opacity: "50%" }}>
+            <p style={{ fontSize: "1.5rem" }}>
+              Enter your password to continue
+            </p>
+            <input
+              type="password"
+              onChange={(e) => {
+                if (e.target.value === PASSWORD) {
+                  setAuthorized(true);
+                }
+              }}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
