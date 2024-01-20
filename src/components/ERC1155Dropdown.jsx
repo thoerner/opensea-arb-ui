d5ae09be4bb2f2190583fcc52b46309b108c116f
Tim Hoerner <thoerner@gmail.com>
2024-01-20 12:20:03 -0500

    show token id in erc1155 token dropdown

--- src/components/ERC1155Dropdown.jsx
+++ src/components/ERC1155Dropdown.jsx
@@ -7,7 +7,7 @@ export const ERC1155Dropdown = ({ items, setToken, value }) => {
       <select name="erc1155-tokens" id="erc1155-tokens" onChange={handleChange} value={value}>
         {items.map(item => (
           <option key={item.identifier} value={item.identifier}>
-            {item.name}
+            {item.name} [Token {item.identifier}]
           </option>
         ))}
       </select>
